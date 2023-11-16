7ae51f0f8f077692827fd4b6002b6113b98d87c4
Silvia Saverino <silviasave89@gmail.com>
2023-11-16 17:59:52 +0000

    remove previous input value

--- index.js
+++ index.js
@@ -2,19 +2,19 @@ const convertBtn = document.getElementById("convert-btn")
 const mainBottom = document.getElementById("main-bottom")
 const inputEl = document.getElementById("input-el")
 
-const unitSection = [
-    "Lenght (Meter/Feet)",
-    "Volume (Liters/Gallons)",
-    "Mass (Kilograms/Pounds)",
-]
+const meterFeets = 3.281
+const literGallons = 0.264
+const kilogramPounds = 2.204
 
-const meterFeets = 3.281 
-const literGallons = 0.264 
-const kilogramPounds = 2.204 
+const unitSection = [
+    "Lenght (Meter to Feet)",
+    "Volume (Liters to Gallons)",
+    "Mass (Kilograms to Pounds)",
+]
 
 function renderUnitSections() {
     let unitDOM = ""
-    
+
     for (let i = 0; i < unitSection.length; i++) {
 
         if (unitSection[i] === unitSection[0]) {
@@ -36,12 +36,12 @@ function renderUnitSections() {
                 <p> ${inputEl.value} Kilograms = ${inputEl.value * kilogramPounds} Pounds</p>
             </div>`
         }
-        
+
     }
     mainBottom.innerHTML = unitDOM
-
 }
 
 convertBtn.addEventListener("click", function () {
     renderUnitSections()
+    inputEl.value = ""
 })
