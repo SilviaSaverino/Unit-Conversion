7fe8140dfc580f459c72339e8c4b5bbd3e91e7f7
Silvia Saverino <silviasave89@gmail.com>
2023-11-16 17:51:01 +0000

    add condition to display conversion results for each unit

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
-/*
-1 meter = 3.281 feet
-1 liter = 0.264 gallon
-1 kilogram = 2.204 pound
-*/
-
 const convertBtn = document.getElementById("convert-btn")
 const mainBottom = document.getElementById("main-bottom")
-
+const inputEl = document.getElementById("input-el")
 
 const unitSection = [
     "Lenght (Meter/Feet)",
@@ -14,22 +8,40 @@ const unitSection = [
     "Mass (Kilograms/Pounds)",
 ]
 
-
+const meterFeets = 3.281 
+const literGallons = 0.264 
+const kilogramPounds = 2.204 
 
 function renderUnitSections() {
     let unitDOM = ""
+    
     for (let i = 0; i < unitSection.length; i++) {
-        unitDOM += `
+
+        if (unitSection[i] === unitSection[0]) {
+            unitDOM += `
         <div id="length" class="unit">
             <h2>${unitSection[i]}</h2>
-            <p>unit will display here</p>
+            <p> ${inputEl.value} Meter = ${inputEl.value * meterFeets} Feet</p>
         </div>`
+        } else if (unitSection[i] === unitSection[1]) {
+            unitDOM += `
+            <div id="length" class="unit">
+                <h2>${unitSection[i]}</h2>
+                <p> ${inputEl.value} Liters = ${inputEl.value * literGallons} Gallons</p>
+            </div>`
+        } else {
+            unitDOM += `
+            <div id="length" class="unit">
+                <h2>${unitSection[i]}</h2>
+                <p> ${inputEl.value} Kilograms = ${inputEl.value * kilogramPounds} Pounds</p>
+            </div>`
+        }
+        
     }
     mainBottom.innerHTML = unitDOM
+
 }
 
-renderUnitSections()
-
-convertBtn.addEventListener("click", function(){
-    console.log('ciao')
+convertBtn.addEventListener("click", function () {
+    renderUnitSections()
 })
